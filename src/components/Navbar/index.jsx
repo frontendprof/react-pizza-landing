ef02317ea93dc7426d976bf6a874beab86b83d71
frontendprof <abdumaliksharipov@gmail.com>
2020-11-25 15:55:30 +0500

    Initial setup and creating several components

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { Nav, NavLink } from "./NavbarElements";
+
+
+
+
+function Navbar() {
+    return (
+        <>
+            <Nav>
+                <NavLink to="/">Pizza</NavLink>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar;
