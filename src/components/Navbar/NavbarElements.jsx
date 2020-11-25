c782958927ccecf70be2e2ac64f5d97b5d4bdb62
frontendprof <abdumaliksharipov@gmail.com>
2020-11-25 16:27:38 +0500

    Creating several style components

--- src/components/Navbar/NavbarElements.jsx
+++ src/components/Navbar/NavbarElements.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { NavLink as Link } from "react-router-dom";
+import { FaPizzaSlice } from "react-icons/fa";
 
 
 
@@ -29,3 +30,24 @@ export const NavLink = styled(Link)`
         left:25px;
     }
 `;
+
+
+export const NavIcon = styled.div`
+    display:block;
+    position:absolute;
+    top:0;
+    right:0;
+    cursor:pointer;
+    color:#fff;
+
+    p{
+        transform:translate(-175%,100%);
+        font-weight:bold;
+    }
+`;
+
+
+export const Bars = styled(FaPizzaSlice)`
+    font-size:2rem;
+    transform:translate(-50%,-15%);
+`
